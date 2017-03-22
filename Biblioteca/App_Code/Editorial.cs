99a7b517b30b53de01e3cef16a8e05290a3f40a3
Lucas <lucas.rojas@live.com.ar>
2017-03-22 20:38:27 -0300

    Se agrego el proyecto de la gestion de libros de una biblioteca en ASP.NET MVC Entity Framework 4

---
+++ Biblioteca/App_Code/Editorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Editorial
+/// </summary>
+public class Editorial
+{
+	public Editorial()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    string editorial;
+
+    public Editorial(string ed){
+
+        editorial = ed;
+    }
+}
---
+++ Biblioteca/App_Code/Genero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Genero
+/// </summary>
+public class Genero
+{
+	public Genero()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    string genero;
+
+    public Genero(string gen){
+
+        genero = gen;
+    }
+}
